8a3eae119ec0dd60023e4142869df79b9f924a7b
Vicky  Tee <vttak9@gmail.com>
2021-01-13 23:20:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -51,6 +51,6 @@ end
 
 def find_the_cheese(array)
   cheeses = ["gouda", "cheddar", "camembert"]
-  array.detect {|food| cheeses.include?(array)}
+  array.find {|food| cheeses.include?(array)}
 
 end
